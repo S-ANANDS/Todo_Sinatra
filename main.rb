e0b2438dc69d88cc6e37d9395b0309cb286489f8
S-ANANDS <anandnair96@gmail.com>
2019-06-03 15:58:42 +0530

    Commented case statement

--- main.rb
+++ main.rb
@@ -4,28 +4,27 @@ get '/' do
   input=params['choice'].to_i
 
 erb :index,locals: {input: input    }
-case input
-when '1'
- 
-when '2'
+# case input
+#   when '1'
   
-when '3'
+#   when '2'
+    
+#   when '3'
+    
+    
+#   when '4'
   
+#   when '5'
   
-when '4'
- 
-when '5'
- 
-when '6'
-  
-when '7'
+#   when '6'
+    
+#   when '7'
 
-when '8'
- 
-else
+#   when '8'
+  
+#   else
+
+# end
+
 
 end
-
-
-end
-
