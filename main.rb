b4dfc605ec6f5b38c2e14d6a8325f804eb6ce8e3
S-ANANDS <anandnair96@gmail.com>
2019-06-06 12:38:32 +0530

    Finished Adding Tasks to server

--- main.rb
+++ main.rb
@@ -1,18 +1,44 @@
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
 get '/' do
-  input=params['choice'].to_i
-  erb :index,locals: {input: input    }
-end
- 
-get '/add' do 
   
-  db=SQLite3::Database.open('todo.db')
-  db.execute 'CREATE TABLE IF NOT EXISTS Tasks(Name VARCHAR)'
-  value=params['task']
-  input=params['choice'].to_i
-  erb :one,locals: {input: input ,value: value   }
-  db.execute "INSERT INTO Tasks (Name) VALUES (?)",[value]
+  erb :index
 end
+
+
+
+
+$db=SQLite3::Database.open('Todo.db')
+$db.execute 'CREATE TABLE IF NOT EXISTS Tasks(Id INTEGER PRIMARY KEY,Name VARCHAR,Status TEXT)'
+
+get '/add' do
+  erb :one
+end
+
+post '/add' do
+  @value = params['task']
+  @state=params['status']
+  $db.execute "INSERT INTO Tasks(Id,Name,Status) VALUES (NULL,?,?)", [@value,@state]
+redirect '/'
+end
+
+get '/result' do
+
+erb :data ,locals:{db: $db}
+end
+
+
+
+
+
+delete '/' do
+end
+patch '/' do
+end
+
+# http://www.purgomalum.com/
+# If user input is
+# If user inpput has profanity, reject it.
